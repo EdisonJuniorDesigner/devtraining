f9f224b0a7a7f9b6b0139584587aa65593ecb714
EdisonJuniorDesigner <mage1232010@gmail.com>
2024-06-19 17:20:20 -0300

    feat: add unit tests and global variables

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -1,18 +1,133 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { CoursesService } from './courses.service';
+import { randomUUID } from 'node:crypto';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { UpdateCourseDto } from './dto/update-course.dto';
 
-describe('CoursesService', () => {
+describe('CoursesService unit tests', () => {
   let service: CoursesService;
+  let id: string;
+  let created_at: Date;
+  let expectOutputTags: any;
+  let expectOutputCourses: any;
+  let mockCoursesRepository: any;
+  let mockTagRepository: any;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CoursesService],
-    }).compile();
+    service = new CoursesService();
+    id = randomUUID();
+    created_at = new Date();
+    expectOutputTags = [
+      {
+        id,
+        name: 'nestjs',
+        created_at,
+      },
+    ];
+    expectOutputCourses = {
+      id,
+      name: 'test',
+      description: 'test description',
+      created_at,
+      tags: expectOutputTags,
+    };
 
-    service = module.get<CoursesService>(CoursesService);
+    mockCoursesRepository = {
+      create: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      save: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      update: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      preload: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      findAll: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      find: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      findOne: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      remove: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+    };
+
+    mockTagRepository = {
+      create: jest.fn().mockReturnValue(Promise.resolve(expectOutputTags)),
+      findOne: jest.fn(),
+    };
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a course', async () => {
+    //@ts-expect-error defined part of methods
+    service['courseRepository'] = mockCoursesRepository;
+    //@ts-expect-error defined part of methods
+    service['tagRepository'] = mockTagRepository;
+
+    const createCourseDTO: CreateCourseDto = {
+      name: 'test',
+      description: 'test description',
+      tags: ['nestjs'],
+    };
+
+    const newCourse = await service.create(createCourseDTO);
+
+    // ToHaveBeenCalled = quer dizer que eu espero que ele tenha sido chamado.
+    expect(mockCoursesRepository.save).toHaveBeenCalled();
+    expect(expectOutputCourses).toStrictEqual(newCourse);
+  });
+
+  it('should list all courses', async () => {
+    //@ts-expect-error defined part of methods
+    service['courseRepository'] = mockCoursesRepository;
+    //@ts-expect-error defined part of methods
+    service['tagRepository'] = mockTagRepository;
+
+    const courses = await service.findAll();
+
+    // ToHaveBeenCalled = quer dizer que eu espero que ele tenha sido chamado.
+    expect(mockCoursesRepository.find).toHaveBeenCalled();
+    expect(expectOutputCourses).toStrictEqual(courses);
+  });
+
+  it('should gets a course by id', async () => {
+    //@ts-expect-error defined part of methods
+    service['courseRepository'] = mockCoursesRepository;
+    //@ts-expect-error defined part of methods
+    service['tagRepository'] = mockTagRepository;
+
+    const course = await service.findOne(id);
+
+    // ToHaveBeenCalled = quer dizer que eu espero que ele tenha sido chamado.
+    expect(mockCoursesRepository.findOne).toHaveBeenCalled();
+    expect(expectOutputCourses).toStrictEqual(course);
+  });
+
+  it('should update a course', async () => {
+    //@ts-expect-error defined part of methods
+    service['courseRepository'] = mockCoursesRepository;
+    //@ts-expect-error defined part of methods
+    service['tagRepository'] = mockTagRepository;
+
+    const updateCourseDTO: UpdateCourseDto = {
+      name: 'test',
+      description: 'test description',
+      tags: ['nestjs'],
+    };
+
+    const course = await service.update(id, updateCourseDTO);
+
+    // ToHaveBeenCalled = quer dizer que eu espero que ele tenha sido chamado.
+    expect(mockCoursesRepository.save).toHaveBeenCalled();
+    expect(mockCoursesRepository.preload).toHaveBeenCalled();
+    expect(expectOutputCourses).toStrictEqual(course);
+  });
+
+  it('should delete a course', async () => {
+    //@ts-expect-error defined part of methods
+    service['courseRepository'] = mockCoursesRepository;
+    //@ts-expect-error defined part of methods
+    service['tagRepository'] = mockTagRepository;
+
+    const course = await service.remove(id);
+
+    // ToHaveBeenCalled = quer dizer que eu espero que ele tenha sido chamado.
+    expect(mockCoursesRepository.findOne).toHaveBeenCalled();
+    expect(mockCoursesRepository.remove).toHaveBeenCalled();
+    expect(expectOutputCourses).toStrictEqual(course);
   });
 });
--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,10 +1,23 @@
-import { DataSource } from 'typeorm';
-import { dataSourceOptions } from './database.module';
+import 'dotenv/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { CreateCoursesTable1718727943704 } from '../migrations/1718727943704-CreateCoursesTable';
 import { CreateTagsTable1718729366482 } from '../migrations/1718729366482-CreateTagsTable';
 import { CreateCourseTagsTable1718730507196 } from '../migrations/1718730507196-CreateCourseTagsTable';
 import { AddCoursesIdToCoursesTagsTable1718730945071 } from '../migrations/1718730945071-AddCoursesIdToCoursesTagsTable';
 import { AddTagsIdToCoursesTagsTable1718731793645 } from '../migrations/1718731793645-AddTagsIdToCoursesTagsTable';
+import { Course } from '../courses/entities/courses.entity';
+import { Tag } from '../courses/entities/tags.entity';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [Course, Tag],
+  synchronize: false,
+};
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
